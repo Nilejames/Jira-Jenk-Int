297589ad43bfa6e58650adb9a011b1166d2d4dfc
Nileena <nileena@netapp.com>
2024-11-25 12:30:12 +0000

    Jira Create Issue #4

--- src/org/netapp/jira/JiraUtils.groovy
+++ src/org/netapp/jira/JiraUtils.groovy
@@ -1,18 +1,17 @@
 package org.netapp.jira
 
 import groovy.json.JsonOutput
+import groovy.json.JsonSlurperClassic
 
 class JiraUtils {
-    static Map getJiraIssueRequestParams(Map params) {
+    static void createIssue(Map params) {
         def jiraUrl = params.jiraUrl
         def projectKey = params.projectKey
         def issueType = params.issueType ?: 'Bug'
         def summary = params.summary
         def description = params.description
-        def jiraUser = params.jiraUser
         def jiraApiToken = params.jiraApiToken
 
-	//def authString = "${jiraApiToken}".bytes.encodeBase64().toString()
         def requestBody = JsonOutput.toJson([
             fields: [
                 project: [
@@ -26,13 +25,16 @@ class JiraUtils {
             ]
         ])
 
-        return [
+        def response = httpRequest(
             httpMode: 'POST',
             url: "${jiraUrl}/rest/api/2/issue",
             contentType: 'APPLICATION_JSON',
             customHeaders: [[name: 'Authorization', value: "Bearer ${jiraApiToken}"]],
-            requestBody: requestBody
-        ]
+            requestBody: requestBody,
+            validResponseCodes: '200:201'
+        )
+
+        def jsonResponse = new JsonSlurperClassic().parseText(response.content)
+        echo "Created JIRA issue: ${jsonResponse.key}"
     }
 }
-
