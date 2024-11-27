98aaf29559e4adefe4a38967ea6ebd63743314bf
Nilejames <nileena.james@netapp.com>
2024-11-27 17:17:34 +0530

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,3 +1,4 @@
+import groovy.json.JsonSlurper
 def call(Map config=[:]) {
   def rawBody = libraryResource 'com/netapp/api/jira/createIssue.json'
   def binding = [
@@ -8,5 +9,25 @@ def call(Map config=[:]) {
     versions:"${config.versions}"
   ]
   def render = renderTemplate(rawBody,binding)
-  sh('curl -D-  -H "Authorization: Bearer "$JIRA_CREDENTIALS -X PUT --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+  def response = sh(
+        script: sh('curl -D-  -H "Authorization: Bearer "$JIRA_CREDENTIALS -X PUT --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue'),
+        returnStdout: true
+    ).trim()
+      // Print the raw response for debugging
+    echo "Raw Response: ${response}"
+    
+    // Parse the response to get the issue key
+    def jsonResponse = new JsonSlurper().parseText(response)
+    def issueKey = jsonResponse.key
+    
+    // Construct the issue URL
+    def issueUrl = "${JIRA_URL}/browse/${issueKey}"
+    
+    // Print the issue key and URL for debugging
+    echo "Issue Key: ${issueKey}"
+    echo "Issue URL: ${issueUrl}"
+    
+    // Return the issue key and URL
+    return [issueKey: issueKey, issueUrl: issueUrl]
+  
 }
