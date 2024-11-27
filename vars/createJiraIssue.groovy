ad1c750479f42d9c5a6214ddce37dcb061fac1df
Nilejames <nileena.james@netapp.com>
2024-11-27 16:14:34 +0530

    Update createJiraIssue.groovy

--- vars/createJiraIssue.groovy
+++ vars/createJiraIssue.groovy
@@ -1,5 +1,13 @@
-// vars/createJiraIssue.groovy
-def call(Map config = [:]) {
-    org.netapp.jira.JiraUtils.createIssue(this, config)
+def call(Map config=[:]) {
+  def rawBody = libraryResource 'com/planetpope/api/jira/createIssue.json'
+  def binding = [
+    key: "${config.key}",
+    summary: "${config.summary}",
+    description: "${config.description}",
+    issueTypeName: "${config.issueTypeName}",
+    labels: "${config.labels}",
+    versions: "${config.versions}"
+  ]
+  def render = renderTemplate(rawBody,binding)
+  sh('curl -D-  -H "Authorization: Bearer "$JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
 }
-
